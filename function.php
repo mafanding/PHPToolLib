9c624414db408d6824271d01485942640e21ad52
mfdboy <mfdboy@163.com>
2016-05-19 15:31:59 +0800

    add function cache

--- function.php
+++ function.php
@@ -1,1 +1,16 @@
-<?php  ?>
+<?php 
+function cache($key,$value=null){
+	if (is_null($value)) {
+		//取缓存	
+		return unserialize(file_get_contents($key));
+	}else{
+		//存缓存
+		$str=serialize($value);
+		if(file_put_contents($key, $str)===false){
+			return false;
+		}else{
+			return true;
+		}
+	}
+}
+?>
